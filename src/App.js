8c14f7e92906b03ad524a598cdeda9717db9f695
sarab-opsera <153240837+sarab-opsera@users.noreply.github.com>
2024-01-11 09:44:30 -0800

    commit Jan 11th Demo

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Fulfillment - Code to Cloud - Opsera - Jan 4th Demo 
+          Fulfillment - Code to Cloud - Opsera - Jan 11th Demo 
         </a>
       </header>
     </div>
