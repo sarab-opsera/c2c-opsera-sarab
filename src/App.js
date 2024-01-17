c584a520598eb33b7340aab9860727f51e58f749
sarab-opsera <153240837+sarab-opsera@users.noreply.github.com>
2024-01-17 12:03:33 -0800

    Demo

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Code to Cloud - Opsera
+          Demo Code to Cloud - Opsera
         </a>
       </header>
     </div>
