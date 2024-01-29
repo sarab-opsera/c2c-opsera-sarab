bb4ca85bf60504e23baa43dc662fbb7b49afd3f3
sarab-opsera <153240837+sarab-opsera@users.noreply.github.com>
2024-01-29 10:42:13 -0800

    Youtube Micro Service

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Amazon</p>
+        <p>Youtube Micro Service</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Demo Jan 10:14
+          Demo Jan  29th 
         </a>
       </header>
     </div>
