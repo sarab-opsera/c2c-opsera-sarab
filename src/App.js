06c44b54c6bc3962d46eaeb2d237e435af1991cf
sarab-opsera <153240837+sarab-opsera@users.noreply.github.com>
2024-06-06 13:43:18 -0700

    Demo June 6th

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Demo May 21st 
+          Demo June 6th 
         </a>
       </header>
     </div>
